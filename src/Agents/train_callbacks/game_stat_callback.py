ce6c1303d8b803787924ce0c86e567e30e542882
nils <nils.eberhardt@posteo.de>
2022-06-26 19:14:36 +0200

    Changed x-axis to episodes in wandb

--- src/Agents/train_callbacks/game_stat_callback.py
+++ src/Agents/train_callbacks/game_stat_callback.py
@@ -19,11 +19,15 @@ class GameStatCallback(BaseCallback):
         before pushing aggregated action frequencies to wandb
         """
         super().__init__()
-        self.action_interval = action_interval
+        self.prefix = 'custom'
 
     def _init_callback(self) -> None:
-        self.actions = []
-        self.none_actions = []
+        self.actions = defaultdict(list)
+        self.none_actions = defaultdict(list)
+        wandb.define_metric('episodes')
+        wandb.define_metric(self.prefix + '/*', step_metric='episodes')
+        self.episodes = 0
+        self.step = 0
 
     def _on_step(self) -> bool:
         """
@@ -31,45 +35,39 @@ class GameStatCallback(BaseCallback):
 
         :return: If the callback returns False, training is aborted early.
         """
-        num_done = 0
-        game_stats = defaultdict(list)
+        game_stats = {}
 
         # We get from locals arrays with length = number of processes used
-        for done, infos in zip(self.locals['dones'], self.locals['infos']):
+        for idx, (done, infos) in enumerate(zip(self.locals['dones'], self.locals['infos'])):
+            self.actions[idx] += infos['actions']
+            self.none_actions[idx] += infos['none_actions']
+
             if done:
-                num_done += 1
-                game_stats['Game Length'].append(infos['game_length'])
-                game_stats['Kore Delta (at End)'].append(infos['kore_delta'])
-                game_stats['Shipyard Count me (at End)'].append(infos['shipyard_count_me'])
-                game_stats['Shipyard Count Opponent (at End)'].append(infos['shipyard_count_opponent'])
-                game_stats['Kore me (at End)'].append(infos['kore_me'])
-                game_stats['Fleet Count me (at End)'].append(infos['fleet_count_me'])
-                game_stats['Fleet Count Opponent (at End)'].append(infos['fleet_count_opponent'])
+                self.episodes += 1
+                game_stats['Game Length'] = infos['game_length']
+                game_stats['Kore Delta (at End)'] = infos['kore_delta']
+                game_stats['Shipyard Count me (at End)'] = infos['shipyard_count_me']
+                game_stats['Shipyard Count Opponent (at End)'] = infos['shipyard_count_opponent']
+                game_stats['Kore me (at End)'] = infos['kore_me']
+                game_stats['Fleet Count me (at End)'] = infos['fleet_count_me']
+                game_stats['Fleet Count Opponent (at End)'] = infos['fleet_count_opponent']
                 kore_delta = infos['kore_delta']
                 my_fleets = infos['fleet_count_me']
                 my_shipyards = infos['shipyard_count_me']
                 win = 1 if (kore_delta > 0 and (my_fleets > 0 or my_shipyards > 0)) else 0
-                game_stats['Winrate'].append(win)
+                game_stats['Winrate'] = win
 
-        # only report something if at least the game of one process is finished
-        if num_done > 0:
-            wandb.log({k: np.mean(v) for k, v in game_stats.items()})
+                counts = Counter(self.actions[idx])
+                action_infos = {'Frequency of ' + key: counts[key] / len(self.actions[idx]) for key in counts}
+                action_infos['Frequency of None Actions'] = sum(self.none_actions[idx]) / len(self.none_actions[idx])
+                game_stats.update(action_infos)
+                self.actions[idx] = []
+                self.none_actions[idx] = []
 
-        # always collect action infos
-        for infos in self.locals['infos']:
-            self.actions += infos['actions']
-            self.none_actions += infos['none_actions']
+                # add prefix (needed to log on episodes)
+                log_dict = {self.prefix + '/' + k: v for k, v in game_stats.items()}
+                log_dict['episodes'] = self.episodes
 
-        # push the action frequencies based on steps and not game ends
-        # to guarantee proportional results
-        if self.n_calls % self.action_interval == 0:
-            counts = Counter(self.actions)
-            action_infos = {'Frequency of ' + key: counts[key]/len(self.actions) for key in counts}
-            # action_infos['Frequency of None Actions'] = sum(self.none_actions)/len(self.none_actions)
-            wandb.log(action_infos)
-            self.actions = []
-            self.none_actions = []
+                wandb.log(log_dict)
 
         return True
-
-
