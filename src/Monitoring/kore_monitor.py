b5219ccf34d145c0372724989fe881ce98a0fb56
nils <nils.eberhardt@posteo.de>
2022-07-02 14:52:40 +0200

    Added chain job support

--- src/Monitoring/kore_monitor.py
+++ src/Monitoring/kore_monitor.py
@@ -11,7 +11,7 @@ ENTITY_NAME: str = os.environ.get(ENTITY_NAME_ENV_NAME)
 
 class KoreMonitor:
 
-    def __init__(self):
+    def __init__(self, run_id: str, resume_training=False):
         if ENTITY_NAME is None:
             raise MissingEntityException(
                 f"No entity name found for WANDB. Please set your environment variable: {ENTITY_NAME_ENV_NAME}"
@@ -24,6 +24,9 @@ class KoreMonitor:
             sync_tensorboard=True,
             monitor_gym=True,
             save_code=True,
+            resume=resume_training,
+            id=run_id
+
         )
 
         self.callback = WandbCallback(
