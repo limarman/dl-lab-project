df5442d6497cfd43b0cd8951dfb9b36e004bc316
nils <nils.eberhardt@posteo.de>
2022-07-14 14:47:06 +0200

    Refactored multimodal transformer class

--- src/Agents/neural_networks/multi_modal_transformer.py
+++ src/Agents/neural_networks/multi_modal_transformer.py
@@ -162,7 +162,9 @@ class MultiModalEmbedding(nn.Module):
                           embedded_patches], dim=1)
 
 """
+***********************************************************************************************************
 Following code has been adapted from https://github.com/lucidrains/vit-pytorch/blob/main/vit_pytorch/vit.py
+***********************************************************************************************************
 """
 
 
@@ -247,79 +249,3 @@ class Transformer(nn.Module):
             x = attn(x) + x
             x = ff(x) + x
         return x
-
-# TODO remove
-class ViT(nn.Module):
-    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool='cls', channels=3,
-                 dim_head=64, dropout=0., emb_dropout=0.):
-        super().__init__()
-        image_height, image_width = pair(image_size)
-        patch_height, patch_width = pair(patch_size)
-
-        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
-
-        num_patches = (image_height // patch_height) * (image_width // patch_width)
-        patch_dim = channels * patch_height * patch_width
-        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
-
-        self.to_patch_embedding = nn.Sequential(
-            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_height, p2=patch_width),
-            nn.Linear(patch_dim, dim),
-        )
-
-        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
-        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
-        self.dropout = nn.Dropout(emb_dropout)
-
-        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
-
-        self.pool = pool
-        self.to_latent = nn.Identity()
-
-        self.mlp_head = nn.Sequential(
-            nn.LayerNorm(dim),
-            nn.Linear(dim, num_classes)
-        )
-
-    def forward(self, img):
-        x = self.to_patch_embedding(img)
-        b, n, _ = x.shape
-
-        cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b=b)
-        x = torch.cat((cls_tokens, x), dim=1)
-        x += self.pos_embedding[:, :(n + 1)]
-        x = self.dropout(x)
-
-        x = self.transformer(x)
-
-        x = x.mean(dim=1) if self.pool == 'mean' else x[:, 0]
-
-        x = self.to_latent(x)
-        return self.mlp_head(x)
-
-batch_size = 2
-game_stats_num = 15
-shipyard_stats = 5
-max_shipyards = 15
-
-feature_maps = torch.rand((batch_size, 15, 21, 21))
-shipyards = torch.randn((batch_size, max_shipyards, shipyard_stats))
-game_stats = torch.randn((batch_size, game_stats_num))
-
-input_dict = {
-            'maps': feature_maps,
-            'scalars': game_stats,
-            'shipyards': shipyards
-        }
-
-spaces = {
-    'maps': gym.spaces.Box(low=-np.Inf, high=np.Inf, shape=(15, 21, 21)),
-    'scalars': gym.spaces.Box(low=-np.Inf, high=np.Inf, shape=(15,)),
-    'shipyards': gym.spaces.Box(low=-np.Inf, high=np.Inf, shape=(15,5))
-}
-
-spaces_dict = gym.spaces.Dict(spaces)
-
-model = MultiModalNet(spaces_dict)
-print(model(input_dict).shape)
-
