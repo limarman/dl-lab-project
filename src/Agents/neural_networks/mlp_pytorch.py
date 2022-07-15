2406984a685c72a7a97c2e232252e624358b77b3
nils <nils.eberhardt@posteo.de>
2022-07-15 14:31:35 +0200

    Added TinyMLP for use in the transformer

--- src/Agents/neural_networks/mlp_pytorch.py
+++ src/Agents/neural_networks/mlp_pytorch.py
@@ -23,3 +23,21 @@ class BasicMLP(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+
+class TinyMLP(nn.Module):
+    """
+    Tiny MLP that is used in the hybrid network
+    """
+
+    def __init__(self, num_input_features: int, num_output_features: int):
+        super().__init__()
+        self.fc1 = nn.Linear(num_input_features, 64)
+        nn.init.kaiming_uniform_(self.fc1.weight, nonlinearity='relu')
+        nn.init.constant_(self.fc1.bias, 0.0)
+        self.fc2 = nn.Linear(64, num_output_features)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
