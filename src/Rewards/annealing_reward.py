19b919ca1559dc8a6b9de11dda3867febea72cfd
Hash-hh <53950589+Hash-hh@users.noreply.github.com>
2022-08-03 04:46:00 +0200

    Added annealing reward

---
+++ src/Rewards/annealing_reward.py
@@ -0,0 +1,60 @@
+from typing import Dict
+from src.Rewards.win_reward import WinReward
+from src.Rewards.advantage_reward import AdvantageReward
+from src.States.kore_state import KoreState
+from src.Rewards.kore_reward import KoreReward
+import numpy as np
+import src.core.global_vars
+
+class AnnealingReward(KoreReward):
+    """
+    Starts from a dense reward and slowly anneals towards a sparse reward.
+    """
+
+    def __init__(self, first_reward=AdvantageReward(), last_reward=WinReward()):
+        """
+        First reward is an initial dense reward.
+        Last reward is the sparse win reward
+        """
+        super().__init__()
+        self.first_reward = first_reward
+        self.last_reward = last_reward
+        self.temp = 50000
+
+        # initialize the step counter
+        src.core.global_vars.init_global_step()
+
+    @staticmethod
+    def get_reward_from_action(current_state: KoreState, actions):
+        return None
+
+    @staticmethod
+    def get_reward_from_states(previous_state: KoreState, next_state: KoreState):
+        return None
+
+    def get_reward(self, previous_state: KoreState, next_state: KoreState, action: Dict[str, str]):
+        """time is the number of time step we are currently at,
+        temp is a constant hyperparameter,
+        It also returns reward info now"""
+
+        time = src.core.global_vars.return_step_count()
+        print("time: ", time)
+
+        first_reward_value = self.first_reward.get_reward(previous_state, next_state, action)
+        last_reward_value = self.last_reward.get_reward(previous_state, next_state, action)
+
+        E = np.e ** -(time/self.temp)
+
+        annealing_reward = first_reward_value * E + last_reward_value * (1 - E)
+
+        annealing_reward_info = {"E": E}
+
+        return annealing_reward, annealing_reward_info
+
+    def reset(self):
+        pass
+
+
+
+
+
